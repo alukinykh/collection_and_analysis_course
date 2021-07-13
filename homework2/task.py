bb038d00739a0f24887b259196734726e0741b20
Anna Lukinykh <avlukinykh@gmail.com>
2021-07-13 17:32:50 +0300

    Homework3

--- homework2/task.py
+++ homework2/task.py
@@ -1,18 +1,16 @@
-# Необходимо собрать информацию о вакансиях на вводимую должность (используем input)
-# с сайтов Superjob(необязательно) и HH(обязательно).
-# Приложение должно анализировать несколько страниц сайта (также вводим через input).
-# Получившийся список должен содержать в себе минимум:
-# 1) Наименование вакансии.
-# 2) Предлагаемую зарплату (отдельно минимальную и максимальную).
-# 3) Ссылку на саму вакансию.
-# 4) Сайт, откуда собрана вакансия.
-#
-# По желанию можно добавить ещё параметры вакансии (например, работодателя и расположение).
-# Структура должна быть одинаковая для вакансий с обоих сайтов. Сохраните результат в json-файл
+"""
+Эта дз является доработкой дз из второго урока; продолжаем работать с hh ru
+1. Развернуть у себя на компьютере/виртуальной машине/хостинге MongoDB и реализовать функцию,
+записывающую собранные вакансии в созданную БД(добавление данных в БД по ходу сбора данных).
+2. Написать функцию, которая производит поиск и выводит на экран вакансии с заработной платой
+больше введённой суммы. Необязательно - возможность выбрать вакансии без указанных зарплат
+3. Написать функцию, которая будет добавлять в вашу базу данных только новые вакансии с сайта.
+"""
 
 import time
 import re
-import json
+import pprint
+from pymongo import MongoClient
 import requests
 from bs4 import BeautifulSoup
 
@@ -20,7 +18,9 @@ from bs4 import BeautifulSoup
 class HHParser:
     """Parse hh.ru"""
     search_link = "https://hh.ru/search/vacancy?text="
-    vacancies = []
+    mongo_client = MongoClient()
+    hh_parser_db = mongo_client.hh_parser_db
+    vacancies_collection = hh_parser_db.vacancies
 
     def __init__(self, position, pages):
         """Init class."""
@@ -58,6 +58,38 @@ class HHParser:
                 pages_count = int(last_pager.text)
         return pages_count
 
+    def parse_compensation(self, compensation):
+        """Parse compensation."""
+        min_compensation = None
+        max_compensation = None
+        compensation_currency = None
+        if compensation is not None:
+            compensation = compensation.text.replace("\u202f", "")
+            if compensation.startswith("до"):
+                compensation_match = re.match(
+                    r"(.*) (?P<compensation>[0-9]+) (?P<currency>.*)",
+                    compensation
+                )
+                max_compensation = int(compensation_match.group("compensation"))
+                compensation_currency = compensation_match.group("currency")
+            elif compensation.startswith("от"):
+                compensation_match = re.match(
+                    r"(.*) (?P<compensation>[0-9]+) (?P<currency>.*)",
+                    compensation
+                )
+                min_compensation = int(compensation_match.group("compensation"))
+                compensation_currency = compensation_match.group("currency")
+            else:
+                compensation_match = re.match(
+                    r"(?P<min>[0-9]+) – (?P<max>[0-9]+) (?P<currency>.*)",
+                    compensation
+                )
+                min_compensation = int(compensation_match.group("min"))
+                max_compensation = int(compensation_match.group("max"))
+                compensation_currency = compensation_match.group("currency")
+
+        return (min_compensation, max_compensation, compensation_currency)
+
     def parse_page(self, page_html):
         """Parse one page."""
         soup = BeautifulSoup(page_html, 'html.parser')
@@ -70,35 +102,18 @@ class HHParser:
             new_vacancy["link"] = title_link["href"]
 
             compensation = vacancy.find("span", {"data-qa": "vacancy-serp__vacancy-compensation"})
-            new_vacancy["min_compensation"] = None
-            new_vacancy["max_compensation"] = None
-            if compensation is not None:
-                compensation = compensation.text.replace("\u202f", "")
-                if compensation.startswith("до"):
-                    new_vacancy["max_compensation"] = compensation[3:]
-                elif compensation.startswith("от"):
-                    new_vacancy["min_compensation"] = compensation[3:]
-                else:
-                    new_vacancy["min_compensation"] = compensation
-                    compensation_match = re.match(
-                        r"(?P<min>[0-9]+) – (?P<max>[0-9]+) (?P<currency>.*)",
-                        compensation
-                    )
-                    currency = compensation_match.group("currency")
-                    new_vacancy["min_compensation"] = compensation_match.group("min") + \
-                                                                            " " + currency
-                    new_vacancy["max_compensation"] = compensation_match.group("max") + \
-                                                                            " " + currency
+            new_vacancy["min_compensation"], new_vacancy["max_compensation"], \
+            new_vacancy["compensation_currency"] = self.parse_compensation(compensation)
 
             new_vacancy["website"] = "hh.ru"
-            self.vacancies.append(new_vacancy)
+            self.vacancies_collection.update_one(new_vacancy, {"$set": new_vacancy}, upsert=True)
 
-    def save_to_json(self, file_path):
-        """Save result to json."""
-        with open(file_path, "w") as json_file:
-            json.dump(self.vacancies, json_file)
+    def print_salary_gt(self, min_salary):
+        """Print vacancies with salary greater than min_salary"""
+        for vacancy in self.vacancies_collection.find({"min_compensation": {"$gt": min_salary}}):
+            pprint.pprint(vacancy)
 
 
 if __name__ == "__main__":
     hh_parser = HHParser(input("Position: "), int(input("Pages: ")))
-    hh_parser.save_to_json("result.json")
+    hh_parser.print_salary_gt(int(input("Print vacancies with salary greater than: ")))
